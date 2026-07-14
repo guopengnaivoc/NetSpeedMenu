9bce6dbeec0f49c2ee9920da5bafb2fb820d61e7
guopengnaivoc <guopeng277051445@gmail.com>
2026-07-13 23:40:10 -0400

    Release NetSpeedMenu 1.3

--- build-installer.sh
+++ build-installer.sh
@@ -5,6 +5,8 @@ ROOT="${0:A:h}"
 APP="$ROOT/outputs/网速.app"
 PACKAGE="$ROOT/outputs/网速安装器.pkg"
 COMPONENT_PACKAGE="$ROOT/work/NetSpeedMenu-component.pkg"
+VERSION=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "$ROOT/Resources/Info.plist")
+VERSIONED_PACKAGE="$ROOT/outputs/NetSpeedMenu-$VERSION-universal.pkg"
 
 "$ROOT/build-app.sh"
 
@@ -14,7 +16,7 @@ rm -f "$COMPONENT_PACKAGE" "$PACKAGE"
 pkgbuild \
     --component "$APP" \
     --identifier "local.codex.NetSpeedMenu.pkg" \
-    --version "1.2" \
+    --version "$VERSION" \
     --ownership recommended \
     --install-location "/Applications" \
     "$COMPONENT_PACKAGE"
@@ -22,5 +24,7 @@ pkgbuild \
 productbuild \
     --package "$COMPONENT_PACKAGE" \
     "$PACKAGE"
+cp "$PACKAGE" "$VERSIONED_PACKAGE"
 
 echo "$PACKAGE"
+echo "$VERSIONED_PACKAGE"
